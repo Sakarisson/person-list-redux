9d429443b758e7fdaa5192a10880a58bb822cfe6
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-09 16:43:44 +0200

    Add removePerson method and tests

--- src/store/reducers/personList.js
+++ src/store/reducers/personList.js
@@ -5,20 +5,28 @@ const baseState = {
 };
 
 export default (state = baseState, action) => {
-  switch (action.types) {
+  switch (action.type) {
     case personListActionTypes.ADD_PERSON:
       return {
         ...state,
         people: [
           ...state.people,
           {
+            id: action.id,
             firstName: action.firstName,
             lastName: action.lastName,
+            address: action.address,
             friendsSortBy: action.friendsSortBy,
             friendsSortOrder: action.friendsSortOrder,
             friends: action.friends,
           },
         ],
+      };
+
+    case personListActionTypes.REMOVE_PERSON:
+      return {
+        ...state,
+        people: state.people.filter(person => person.id !== action.id),
       };
     default:
       return state;
---
+++ src/store/reducers/personList.spec.js
@@ -0,0 +1,59 @@
+import personList from './personList';
+import * as personListActions from '../actions/personListActions';
+
+describe('personList reducer', () => {
+  it('should handle initial state', () => {
+    expect(personList(undefined, {})).toEqual({ people: [] });
+  });
+
+  it('should handle ADD_PERSON', () => {
+    const person = {
+      id: '1',
+      firstName: 'Foo',
+      lastName: 'Bar',
+      address: { streetAddress: 'Homeplace 5', city: 'Helsinki', zipCode: '12321' },
+      friendsSortBy: 'none',
+      friendsSortOrder: 'ascending',
+      friends: ['2'],
+    };
+    expect(personList(undefined, personListActions.addPerson(person))).toEqual({
+      people: [person],
+    });
+  });
+
+  it('should handle REMOVE_PERSON', () => {
+    const people = [
+      {
+        id: '1',
+        firstName: 'Foo',
+        lastName: 'Bar',
+        address: { streetAddress: 'Homeplace 5', city: 'Helsinki', zipCode: '12321' },
+        friendsSortBy: 'none',
+        friendsSortOrder: 'ascending',
+        friends: ['2'],
+      },
+      {
+        id: '2',
+        firstName: 'Bar',
+        lastName: 'Foo',
+        address: { streetAddress: 'Somewhere 3', city: 'Porvoo', zipCode: '32323' },
+        friendsSortBy: 'name',
+        friendsSortOrder: 'descending',
+        friends: ['1'],
+      },
+    ];
+    expect(personList({ people }, personListActions.removePerson('1'))).toEqual({
+      people: [
+        {
+          id: '2',
+          firstName: 'Bar',
+          lastName: 'Foo',
+          address: { streetAddress: 'Somewhere 3', city: 'Porvoo', zipCode: '32323' },
+          friendsSortBy: 'name',
+          friendsSortOrder: 'descending',
+          friends: ['1'],
+        },
+      ],
+    });
+  });
+});
