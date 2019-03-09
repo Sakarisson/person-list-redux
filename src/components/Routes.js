cdbaecc3b95a5a9f82fe75b1e81f75718e79f51c
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-09 18:13:21 +0200

    Add people to routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
+import PersonPage from './PersonPage';
 import Home from './Home';
 
-const Routes = () => [<Route exact path="/" component={Home} key="home" />];
+const Routes = ({ people }) => [
+  <Route exact path="/" component={Home} key="home" />,
+  ...people.map(person => (
+    <Route
+      exact
+      path={`/people/${person.id}`}
+      component={() => <PersonPage person={person} />}
+      key={person.id}
+    />
+  )),
+];
 
-export default Routes;
+export default compose(
+  withRouter,
+  connect(state => ({
+    people: state.personList.people,
+  })),
+)(Routes);
