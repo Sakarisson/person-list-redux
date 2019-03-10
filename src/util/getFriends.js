433e524be2526116a2c66173170b144944fea892
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-10 11:21:53 +0200

    Add sorting to getFriends

--- src/util/getFriends.js
+++ src/util/getFriends.js
@@ -1,6 +1,21 @@
-const getFriends = (state, id) =>
+const sortByLenses = {
+  none: () => false,
+  name: ({ firstName, lastName }) => `${firstName} ${lastName}`,
+  city: ({ address: { city } }) => city,
+};
+
+const comparisonOperators = {
+  ascending: lens => (a, b) => lens(a) > lens(b),
+  descending: lens => (a, b) => lens(a) < lens(b),
+};
+
+const getComparisonFunction = (sortBy, sortOrder) =>
+  comparisonOperators[sortOrder](sortByLenses[sortBy]);
+
+const getFriends = (state, id, sortBy = 'none', sortOrder = 'ascending') =>
   state.personList.people
     .find(p => p.id === id)
-    .friends.map(f => state.personList.people.find(p => p.id === f));
+    .friends.map(f => state.personList.people.find(p => p.id === f))
+    .sort(getComparisonFunction(sortBy, sortOrder));
 
 export default getFriends;
--- src/util/getFriends.spec.js
+++ src/util/getFriends.spec.js
@@ -49,4 +49,181 @@ describe('getFriends', () => {
     };
     expect(getFriends(state, '1')).toEqual([]);
   });
+
+  describe('it can sort by name', () => {
+    it('in ascending order', () => {
+      const state = {
+        personList: {
+          people: [
+            {
+              id: '1',
+              firstName: 'person1',
+              friends: ['2', '3'],
+            },
+            {
+              id: '2',
+              firstName: 'person2',
+              friends: ['1', '3'],
+            },
+            {
+              id: '3',
+              firstName: 'person3',
+              friends: ['1', '2'],
+            },
+          ],
+        },
+      };
+      expect(getFriends(state, '1', 'name', 'ascending')).toEqual([
+        {
+          id: '2',
+          firstName: 'person2',
+          friends: ['1', '3'],
+        },
+        {
+          id: '3',
+          firstName: 'person3',
+          friends: ['1', '2'],
+        },
+      ]);
+    });
+
+    it('in descending order', () => {
+      const state = {
+        personList: {
+          people: [
+            {
+              id: '1',
+              firstName: 'person1',
+              friends: ['2', '3'],
+            },
+            {
+              id: '2',
+              firstName: 'person2',
+              friends: ['1', '3'],
+            },
+            {
+              id: '3',
+              firstName: 'person3',
+              friends: ['1', '2'],
+            },
+          ],
+        },
+      };
+      expect(getFriends(state, '1', 'name', 'descending')).toEqual([
+        {
+          id: '3',
+          firstName: 'person3',
+          friends: ['1', '2'],
+        },
+        {
+          id: '2',
+          firstName: 'person2',
+          friends: ['1', '3'],
+        },
+      ]);
+    });
+  });
+  describe('it can sort by city', () => {
+    it('in ascending order', () => {
+      const state = {
+        personList: {
+          people: [
+            {
+              id: '1',
+              firstName: 'person1',
+              address: {
+                city: 'city1',
+              },
+              friends: ['2', '3'],
+            },
+            {
+              id: '2',
+              firstName: 'person2',
+              address: {
+                city: 'city2',
+              },
+              friends: ['1', '3'],
+            },
+            {
+              id: '3',
+              firstName: 'person3',
+              address: {
+                city: 'city3',
+              },
+              friends: ['1', '2'],
+            },
+          ],
+        },
+      };
+      expect(getFriends(state, '1', 'city', 'ascending')).toEqual([
+        {
+          id: '2',
+          firstName: 'person2',
+          address: {
+            city: 'city2',
+          },
+          friends: ['1', '3'],
+        },
+        {
+          id: '3',
+          firstName: 'person3',
+          address: {
+            city: 'city3',
+          },
+          friends: ['1', '2'],
+        },
+      ]);
+    });
+
+    it('in descending order', () => {
+      const state = {
+        personList: {
+          people: [
+            {
+              id: '1',
+              firstName: 'person1',
+              address: {
+                city: 'city1',
+              },
+              friends: ['2', '3'],
+            },
+            {
+              id: '2',
+              firstName: 'person2',
+              address: {
+                city: 'city2',
+              },
+              friends: ['1', '3'],
+            },
+            {
+              id: '3',
+              firstName: 'person3',
+              address: {
+                city: 'city3',
+              },
+              friends: ['1', '2'],
+            },
+          ],
+        },
+      };
+      expect(getFriends(state, '1', 'city', 'descending')).toEqual([
+        {
+          id: '3',
+          firstName: 'person3',
+          address: {
+            city: 'city3',
+          },
+          friends: ['1', '2'],
+        },
+        {
+          id: '2',
+          firstName: 'person2',
+          address: {
+            city: 'city2',
+          },
+          friends: ['1', '3'],
+        },
+      ]);
+    });
+  });
 });
