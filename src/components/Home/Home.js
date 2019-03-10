4753ae86a8c7dc6dbdcfb02c8f5d4ffb9cad960d
Kristian Sakarisson <kristian@sakarisson.com>
2019-03-10 14:05:54 +0200

    Implement generateFriends

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withHandlers, compose } from 'recompose';
+import { min as _min, shuffle as _shuffle } from 'lodash';
 
 import PersonList from '../PersonList';
 import AddPeople from './AddPeople';
-import { addPerson, removePerson } from '../../store/actions/personListActions';
+import {
+  addPerson as addPersonAction,
+  removePerson as removePersonAction,
+  clearAllFriends as clearAllFriendsAction,
+  addFriend as addFriendAction,
+} from '../../store/actions/personListActions';
 
-const Home = ({ personList, addPerson, removePerson }) => (
+const Home = ({ personList, addPerson, removePerson, generateFriendships }) => (
   <div>
     <AddPeople addPerson={addPerson} />
+    {personList.people.length > 1 && (
+      <button type="button" onClick={() => generateFriendships(5)}>
+        Generate friendships
+      </button>
+    )}
     {!!personList.people.length && (
       <Fragment>
         <p>People:</p>
@@ -22,14 +34,42 @@ Home.propTypes = {
   personList: PropTypes.shape({
     people: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
+  addPerson: PropTypes.func.isRequired,
+  removePerson: PropTypes.func.isRequired,
+  generateFriendships: PropTypes.func.isRequired,
 };
 
-export default connect(
-  state => ({
-    personList: state.personList,
-  }),
-  dispatch => ({
-    addPerson: person => dispatch(addPerson(person)),
-    removePerson: id => dispatch(removePerson(id)),
+export default compose(
+  connect(
+    state => ({
+      personList: state.personList,
+    }),
+    dispatch => ({
+      addPerson: person => dispatch(addPersonAction(person)),
+      removePerson: id => dispatch(removePersonAction(id)),
+      clearAllFriends: () => dispatch(clearAllFriendsAction()),
+      addFriend: (personId, friendId) => dispatch(addFriendAction(personId, friendId)),
+    }),
+  ),
+  withHandlers({
+    generateFriendships: ({ clearAllFriends, addFriend, personList }) => (numberOfFriends = 1) => {
+      clearAllFriends();
+      personList.people.forEach(p => {
+        const potentialNewFriends = personList.people.filter(
+          other =>
+            !other.friends.some(f => f === p.id) &&
+            other.friends.length < numberOfFriends &&
+            other.id !== p.id,
+        );
+        const initialFriendCount = p.friends.length;
+        const potentialNewFriendsCount = potentialNewFriends.length;
+        if (initialFriendCount >= numberOfFriends || potentialNewFriendsCount === 0) {
+          return;
+        }
+        const toBeAdded = _min([numberOfFriends, potentialNewFriendsCount]);
+        const newFriends = _shuffle(potentialNewFriends).slice(0, toBeAdded);
+        newFriends.forEach(f => addFriend(p.id, f.id));
+      });
+    },
   }),
 )(Home);
